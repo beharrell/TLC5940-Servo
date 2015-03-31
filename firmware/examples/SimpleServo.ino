139eb035fe15859729f81971b4c3ea7aaf65e337
beharrell <mark.beharrell@gmail.com>
2015-03-31 21:08:50 +1300

    working example

--- firmware/examples/SimpleServo.ino
+++ firmware/examples/SimpleServo.ino
@@ -1,4 +1,4 @@
-#include "TLC5940-Servo.h"
+#include "TLC5940-Servo/TLC5940-Servo.h"
 
 
 void setup()
@@ -10,7 +10,7 @@ int angle = 0;
 void loop()
 {
 	angle = angle >= 180 ? 0 : angle + 1;
-    TLC5940Servo::SetChannel(0, degrees);
+    TLC5940Servo::SetChannel(0, angle);
 	TLC5940Servo::Update();
 	delay(500); 
 }
